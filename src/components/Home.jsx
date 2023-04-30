3fde3b854800cd9db8b92a3ee2935b1abd534aed
pragatikohli <pragatikohli12@gmail.com>
2023-04-30 15:51:57 +0530

    home component updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Header from './Header'
-import Code from './Code'
+import Code from './Code';
+import Result from './Result';
+import Header from './Header';
 
-function Home() {
-  return (
-    <>
-        <Header/>
-        <Code/>
-    </>
-  )
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Code />
+            <Result />
+        </>
+    )
 }
 
-export default Home
+export default Home;
